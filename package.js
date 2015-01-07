fe8c90546b3ca1d68d366650175bfb40ab313ce1
Vadim Lopatyuk <qnub.ru@gmail.com>
2015-01-07 11:56:19 +0600

    initial release

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'qnub:emojione',
+  summary: 'Meteor Package of http://www.emojione.com/ set',
+  version: '0.0.1',
+  git: 'https://github.com/qnub/emojione.git',
+  name: "qnub:emojione"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2.1');
+  api.addFiles('assets/css/emojione.css', 'client');
+
+  api.use('templating', 'client');
+  api.addFiles('lib/js/emojione.js', ['client']);
+  api.addFiles('qnub:emojione.js', ['client']);
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('qnub:emojione');
+  api.addFiles('qnub:emojione-tests.js');
+});
